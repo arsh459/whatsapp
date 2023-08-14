ee219a4745ba5782b1ac89acedcad3c7e6756c62
arsh459 <arshgoyal459@gmail.com>
2023-08-14 15:14:04 +0530

    middlewares

--- whatsapp_be/src/app.js
+++ whatsapp_be/src/app.js
@@ -1,10 +1,35 @@
 import express from "express"
+import dotenv from "dotenv"
+import morgan from "morgan"
+import helmet from "helmet"
+import mongoSanitize from "express-mongo-sanitize"
+import cookieParser from "cookie-parser"
+import compression from "compression"
+import fileUpload from "express-fileupload"
+import cors from "cors"
+
+// dotenv config
+dotenv.config()
 
 // create express app
 const app = express()
 
+// middlewares
+if (process.env.NODE_ENV !== "production") {
+  app.use(morgan("dev"))
+}
+
+app.use(helmet())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(mongoSanitize())
+app.use(cookieParser())
+app.use(compression())
+app.use(fileUpload({ useTempFiles: true }))
+app.use(cors())
+
 app.get("/", (req, res) => {
-  res.send("hello")
+  res.send(req.body)
 })
 
 export default app
--- whatsapp_be/src/index.js
+++ whatsapp_be/src/index.js
@@ -1,11 +1,8 @@
-import dotenv from "dotenv"
 import app from "./app.js"
-
-// dotenv config
-dotenv.config()
 
 // env variables
 const PORT = process.env.PORT || 8000
+console.log(process.env.NODE_ENV)
 
 app.listen(PORT, () => {
   console.log(`server is listening at ${PORT}`)
